c73cd4f30b141b54ac2d744a40280e08a6831b7d
zychai <>
2024-07-17 16:34:58 +0800

    demo

---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Check if Bundler is installed
+if ! command -v bundle &> /dev/null
+then
+    echo "Bundler is not installed. Install Bundler first."
+    exit
+fi
+
+# Output the current directory
+echo "The current directory is:$(pwd)"
+
+# Install the necessary gem packages
+bundle install
+
+# Start Jekyll local server
+bundle exec jekyll serve
